7f84183f9aa55e9f6a7d65b3f2c44ebcee896497
NicoliIavanco <nicoli.avanco@escola.pr.gov.br>
2025-08-01 09:55:18 -0300

    Update perguntas.js

--- flashcard-aula_10/perguntas.js
+++ flashcard-aula_10/perguntas.js
@@ -1,23 +1,101 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'História',
+    'Quem foi o responsável pela abolição da escravatura no Brasil?',
+    'Princesa Isabel'
 )
-
+criaCartao(
+    'História',
+    'Em que ano ocorreu a Proclamação da República no Brasil?  ',
+    '1889'
+)
+criaCartao(
+    'História',
+    'Quem foi o líder da Revolução Russa de 1917? ',
+    'Vladimir Lênin    '
+)
+criaCartao(
+    'História',
+    'Qual tratado encerrou a Primeira Guerra Mundial?',
+    'Tratado de Versalhes  '
+)
+criaCartao(
+    'História',
+    'Quem foi o primeiro imperador do Brasil?',
+    'Dom Pedro I' 
+)
 criaCartao(
     'Geografia',
     'Qual a capital da França?',
     'A capital da França é Paris'
 )
-
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Geografia',
+    'Qual é o maior país do mundo em extensão territorial?',
+    'Rússia'
+)
+criaCartao(
+    'Geografia',
+    'Qual é o ponto mais alto do Brasil? ',
+    'Pico da Neblina'
+)
+criaCartao(
+    'Geografia',
+    'Qual continente é conhecido como o berço da humanidade? ',
+    'Africa'
+)
+criaCartao(
+    'Geografia',
+    'Qual é o maior oceano do planeta? ',
+    'Oceano Pacífico'
 )
 
 criaCartao(
-    'Lingua inglesa',
+    'Matemática',
+    '7x8?',
+    '56'
+)
+criaCartao(
+    'Matemática',
+    '100/2',
+    '50'
+)
+criaCartao(
+    'Matemática',
+    '15+25',
+    '40'
+)
+criaCartao(
+    'Matemática',
+    '32+54',
+    '86'
+)
+criaCartao(
+    'Matemática',
+    '5x9',
+    '45'
+)
+criaCartao(
+    'Esportes',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Esportes',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Esportes',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Esportes',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Esportes',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
