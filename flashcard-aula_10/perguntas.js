dfc32caa19fb63f72a2ed8e199f47b05ac739a9e
NicoliIavanco <nicoli.avanco@escola.pr.gov.br>
2025-08-05 08:14:40 -0300

    Update perguntas.js

--- flashcard-aula_10/perguntas.js
+++ flashcard-aula_10/perguntas.js
@@ -76,26 +76,51 @@ criaCartao(
 )
 criaCartao(
     'Esportes',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Quem é o maior artilheiro da história da Seleção Brasileira?',
+    'pelé'
 )
 criaCartao(
     'Esportes',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Qual país venceu a Copa do Mundo de 2022?',
+    'Argentina'
 )
 criaCartao(
     'Esportes',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Quem foi o técnico do Brasil na Copa do Mundo de 2002, quando conquistou o penta? ',
+    'Luiz Felipe Scolari (Felipão)'
 )
 criaCartao(
     'Esportes',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Qual é o nome do estádio mais famoso da cidade do Rio de Janeiro?',
+    ' Maracanã.'
 )
 criaCartao(
     'Esportes',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Qual clube brasileiro tem mais títulos da Copa Libertadores? ',
+    ' Futebol Clube (empatado com outros em número de títulos até 2023, como Flamengo e Palmeiras).'
+)
+criaCartao(
+    'filmes',
+    'Qual filme ganhou o Oscar de Melhor Filme em 2023?',
+    'Tudo em Todo Lugar ao Mesmo Tempo.'
+)
+criaCartao(
+    'Filmes',
+    'Quem dirigiu o filme Titanic? ',
+    ' James Cameron.'
+)
+criaCartao(
+    'Filmes',
+    'Qual é o nome do vilão principal em Os Vingadores (2012)?  ',
+    'Loki'
+)
+criaCartao(
+    'Filmes',
+    'Qual filme da franquia Harry Potter foi o último a ser lançado? ',
+    ' Harry Potter e as Relíquias da Morte – Parte 2.'
+)
+criaCartao(
+    'Filmes',
+    'Qual é o nome do robô protagonista em Wall-E? ',
+    'Wall-E'
 )
